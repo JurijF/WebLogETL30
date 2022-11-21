4a75dafa016b32885b5564aa55da784d515fea9e
Tim Holstein <nyxhimself@gmx.de>
2022-11-21 20:59:08 +0100

    Add project files.

---
+++ WpfApp1fewfwef/MainWindow.xaml.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Runtime.InteropServices;
+using System.Windows.Interop;
+
+
+namespace WpfApp1fewfwef
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private bool mRestoreIfMove = false;
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.MaxWidth = System.Windows.SystemParameters.PrimaryScreenWidth;
+            this.MaxHeight = System.Windows.SystemParameters.PrimaryScreenHeight;
+            //Rectangle resolution = Screen.PrimaryScreen.Bounds;
+            //MaxHeight = SystemParameters.VirtualScreenHeight;
+            //MaxWidth = SystemParameters.VirtualScreenWidth;
+            //this.Height = SystemParameters.MaximizedPrimaryScreenHeight;
+            test.Items.Add("Analyse 1");
+            test.Items.Add("Analyse 2");
+            test.Items.Add("Analyse 3");
+            test.Items.Add("Analyse 4");
+            test.SelectedIndex = 0;
+            keke.Items.Add("test");
+            test1.Text = this.MaxHeight.ToString();
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void btn_Create_Login_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ToggleButton_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            kek.Text = kek.Text;
+        }
+
+        private void btnMin_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+       
+
+        private void rctHeader_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.WindowState == WindowState.Normal)
+            {
+                this.WindowState = WindowState.Maximized;
+            }
+            else if (this.WindowState == WindowState.Maximized)
+            {
+                this.WindowState = WindowState.Normal;
+            }
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+    }
+}
