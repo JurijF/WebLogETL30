977a6a14929ff1021473b8a8fc4dfba11c21d56a
Malte Tramsen <malte.tramsen@gmx.de>
2022-11-29 10:55:31 +0100

    Implemented SchemaCheck when selecting a new database

--- WebLogETL30/SettingsForm.cs
+++ WebLogETL30/SettingsForm.cs
@@ -20,8 +20,22 @@ namespace WebLogETL30
 
         private void CreateTables()
         {
-            DBHandler.NonQuery("CREATE TABLE ImportedFiles( ID INTEGER PRIMARY KEY AUTOINCREMENT, FileHash VARCHAR(32), Timestamp DATETIME DEFAULT CURRENT_TIMESTAMP); ");
+            DBHandler.NonQuery("CREATE TABLE ImportedFiles( ID INTEGER PRIMARY KEY AUTOINCREMENT, FileHash VARCHAR(32), Timestamp DATETIME DEFAULT CURRENT_TIMESTAMP);");
             DBHandler.NonQuery("CREATE TABLE Logs ( ID INTEGER PRIMARY KEY   AUTOINCREMENT, Hash VARCHAR(32) NOT NULL, TimestampInsert DATETIME DEFAULT CURRENT_TIMESTAMP, IP TEXT(128), DT_EVENT DATETIME, TYP VARCHAR(32), EVENT TEXT(8192), Status INTEGER, Number INTEGER);");
+        }
+
+        private bool CheckDbSchema(string dbPath)
+        {
+            string oldFilePath = Properties.Settings.Default.DB_FILE;
+            Properties.Settings.Default.DB_FILE = dbPath;
+            string dbConnection = @"Data Source=" + dbPath + ";Version=3;New=True;Compress=True;";
+            DataTable checkTable = DBHandler.ExecuteQuery("SELECT * FROM sqlite_master WHERE type = 'table' ORDER BY 1");
+            DataRow[] tableInfos = checkTable.Select("name IN ('Logs','ImportedFiles')");
+            if (tableInfos.Length!=2){ Properties.Settings.Default.DB_FILE = oldFilePath; return false; }
+            if (tableInfos[0][4].ToString()!= "CREATE TABLE ImportedFiles( ID INTEGER PRIMARY KEY AUTOINCREMENT, FileHash VARCHAR(32), Timestamp DATETIME DEFAULT CURRENT_TIMESTAMP)") { Properties.Settings.Default.DB_FILE = oldFilePath; return false; }
+            if (tableInfos[1][4].ToString()!= "CREATE TABLE Logs ( ID INTEGER PRIMARY KEY   AUTOINCREMENT, Hash VARCHAR(32) NOT NULL, TimestampInsert DATETIME DEFAULT CURRENT_TIMESTAMP, IP TEXT(128), DT_EVENT DATETIME, TYP VARCHAR(32), EVENT TEXT(8192), Status INTEGER, Number INTEGER)") { Properties.Settings.Default.DB_FILE = oldFilePath; return false; }
+            
+            return true;
         }
         #endregion Methods
         #region Events
@@ -55,7 +69,14 @@ namespace WebLogETL30
 
             if (openFileDialog.ShowDialog() == DialogResult.OK)
             {
-                Properties.Settings.Default.DB_FILE = openFileDialog.FileName;
+                if(!CheckDbSchema(openFileDialog.FileName))
+                {
+                    MessageBox.Show("Diese Datenbank ist nicht kompatibel");
+                }
+                else
+                {
+                    Properties.Settings.Default.DB_FILE = openFileDialog.FileName;
+                }                
             }
             txtBox_settings_SelectedDB.Text = Properties.Settings.Default.DB_FILE;
         }
