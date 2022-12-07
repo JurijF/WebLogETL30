7d7244b1334be55cecb440dfe88a58a95691daf0
Tim Holstein <nyxhimself@gmx.de>
2022-12-08 00:16:31 +0100

    almost done

--- WpfApp1fewfwef/MainWindow.xaml.cs
+++ WpfApp1fewfwef/MainWindow.xaml.cs
@@ -24,8 +24,8 @@ namespace WpfApp1fewfwef
     public partial class MainWindow : Window
     {
         private bool mRestoreIfMove = false;
-        public int CurrentWindowWidth = Convert.ToInt32(Convert.ToDouble(System.Windows.SystemParameters.PrimaryScreenWidth * 0.5));
-        public int CurrentWindowHeight = Convert.ToInt32(Convert.ToDouble(System.Windows.SystemParameters.PrimaryScreenHeight * 0.5));
+        public int CurrentWindowWidth = Convert.ToInt32(Convert.ToDouble(System.Windows.SystemParameters.PrimaryScreenWidth * 0.8));
+        public int CurrentWindowHeight = Convert.ToInt32(Convert.ToDouble(System.Windows.SystemParameters.PrimaryScreenHeight * 0.8));
         public MainWindow()
         {
             InitializeComponent();
@@ -147,14 +147,14 @@ namespace WpfApp1fewfwef
             this.panel_1.Width = this.border_3.Width;
             this.txt_bl_Zeitraum.Width = this.panel_1.Width * 0.976;
             this.border_4.Width = this.panel_1.Width * 0.94;
-            this.column_def_2_1.Width = new GridLength(this.border_4.Width * 0.04255, GridUnitType.Star);
+            this.column_def_2_1.Width = new GridLength(this.border_4.Width * 0.06255, GridUnitType.Star);
             this.column_def_2_2.Width = new GridLength(this.border_4.Width * 0.08510, GridUnitType.Star);
             this.column_def_2_3.Width = new GridLength(this.border_4.Width * 0.17021, GridUnitType.Star);
             this.column_def_2_4.Width = new GridLength(this.border_4.Width * 0.14894, GridUnitType.Star);
             this.column_def_2_5.Width = new GridLength(this.border_4.Width * 0.08510, GridUnitType.Star);
             this.column_def_2_6.Width = new GridLength(this.border_4.Width * 0.17021, GridUnitType.Star);
             this.column_def_2_7.Width = new GridLength(this.border_4.Width * 0.14894, GridUnitType.Star);
-            this.column_def_2_8.Width = new GridLength(this.border_4.Width * 0.14895, GridUnitType.Star);
+            
             this.icon_DateRange_1.Width = this.border_4.Width * 0.04255;
             this.txt_bl_bis.Width = this.border_4.Width * 0.14894;
             this.icon_TimeRange_1.Width = this.border_4.Width * 0.17021;
@@ -192,6 +192,8 @@ namespace WpfApp1fewfwef
             this.grid_1.Width = this.btn_import.Width;
             this.border_9.Width = this.border_8.Width;
             this.btn_load.Width = this.border_9.Width;
+            this.column_def_5_1.Width = new GridLength(30, GridUnitType.Star);
+            this.column_def_5_2.Width = new GridLength(70, GridUnitType.Star);
             this.column_def_8_1.Width = new GridLength(30, GridUnitType.Star);
             this.column_def_8_2.Width = new GridLength(70, GridUnitType.Star);
             this.column_def_10_1.Width = new GridLength(30, GridUnitType.Star);
@@ -202,6 +204,34 @@ namespace WpfApp1fewfwef
             this.column_def_12_2.Width = new GridLength(70, GridUnitType.Star);
             this.column_def_13_1.Width = new GridLength(30, GridUnitType.Star);
             this.column_def_13_2.Width = new GridLength(70, GridUnitType.Star);
+            this.column_def_14_1.Width = new GridLength(90.25, GridUnitType.Star);
+            this.column_def_14_2.Width = new GridLength(4, GridUnitType.Star);
+            this.column_def_14_3.Width = new GridLength(1.75, GridUnitType.Star);
+            this.column_def_14_4.Width = new GridLength(4, GridUnitType.Star);
+
+            this.btnExit.Width = 30;
+            this.btnExit.Height = 30;
+            this.icon_close.Width = 30;
+            this.icon_close.Height = 30;
+            this.btnExit.Margin = new Thickness(0, 10, 0, 0);
+            this.icon_close.Margin = new Thickness(-17, -15, -14, -15);
+
+            this.rctHeader.Width = 30;
+            this.rctHeader.Height = 30;
+            this.icon_max.Width = 30;
+            this.icon_max.Height = 30;
+            this.rctHeader.Margin = new Thickness(0, 10, 0, 0);
+            this.icon_max.Margin = new Thickness(-17, -15, -14, -15);
+
+            this.btnMin.Width = 30;
+            this.btnMin.Height = 30;
+            this.icon_min.Width = 30;
+            this.icon_min.Height = 30;
+            this.btnMin.Margin = new Thickness(0, 10, 0, 0);
+            this.icon_min.Margin = new Thickness(-17, -15, -14, -15);
+
+            
+
             //test1.Text = this.border_6.Width.ToString();
 
         }
@@ -213,7 +243,9 @@ namespace WpfApp1fewfwef
             //450 
             this.Height = this.CurrentWindowHeight;
             this.border_1.Height = this.Height;
-            
+
+            this.test1.FontSize = this.Height * 0.02;
+
             this.row_def_1_1.Height = new GridLength(4.44, GridUnitType.Star);
             this.row_def_1_2.Height = new GridLength(95.56, GridUnitType.Star);
             this.row_def_2_1.Height = new GridLength(73, GridUnitType.Star);
@@ -256,9 +288,15 @@ namespace WpfApp1fewfwef
             this.datagrid_typ.Width = (this.border_2.Width - 5) / 6;
             this.datagrid_number.Width = (this.border_2.Width - 5) / 6;
 
+            double testings = 0;
+            Loaded += (s, e) =>
+            {
+                testings = this.column_def_5_1.ActualWidth * 0.020202;
+            };
+
             this.ckbx_IP.MinHeight = this.panel_FirstAnalyse.Height / 100 * 0.1666;
             this.ckbx_IP.MinWidth = this.panel_FirstAnalyse.Width / 100 * 0.1666;
-            this.ckbx_IP.LayoutTransform = new ScaleTransform(this.panel_FirstAnalyse.Width / 165.0, this.panel_FirstAnalyse.Height / 140.0);
+            this.ckbx_IP.LayoutTransform = new ScaleTransform(this.panel_FirstAnalyse.Width * 0.0060606, this.panel_FirstAnalyse.Height / 140.0);
             this.ckbx_IP.Margin = new Thickness(this.Width * 0.02, this.Height * 0.04, 0, 0);
             this.ckbx_IP.Width = this.panel_FirstAnalyse.Width * 0.04137;
             this.ckbx_IP.Height = this.panel_FirstAnalyse.Height * 0.04137;
@@ -267,8 +305,8 @@ namespace WpfApp1fewfwef
             #region Fix Checkbox-scaling-bug
 
             this.ckbx_Timestamp.MinHeight = this.panel_FirstAnalyse.Height / 100 * 0.1666;
-            this.ckbx_Timestamp.MinWidth = this.panel_FirstAnalyse.Width * 0.006;
-            this.ckbx_Timestamp.LayoutTransform = new ScaleTransform(this.panel_FirstAnalyse.Width * 0.006, this.panel_FirstAnalyse.Height / 140);
+            this.ckbx_Timestamp.MinWidth = this.panel_FirstAnalyse.Width * 0.1666;
+            this.ckbx_Timestamp.LayoutTransform = new ScaleTransform(this.panel_FirstAnalyse.Width / 165, this.panel_FirstAnalyse.Height / 140);
             this.ckbx_Timestamp.Margin = new Thickness(this.Width * 0.02, 0, 0, 0);
             this.ckbx_Timestamp.Width = this.panel_FirstAnalyse.Width * 0.05137;
             this.ckbx_Timestamp.Height = this.panel_FirstAnalyse.Height * 0.04137;
@@ -402,7 +440,47 @@ namespace WpfApp1fewfwef
             this.txt_bl_load.FontSize = this.grid_3.Width * 0.13;
             this.txt_bl_load.Margin = new Thickness(0, 0, this.Height * 0.03, 0);
 
-            this.test1.Text = Convert.ToInt32(Convert.ToDouble(System.Windows.SystemParameters.PrimaryScreenWidth * 0.8)) + " x " + Convert.ToInt32(Convert.ToDouble(System.Windows.SystemParameters.PrimaryScreenHeight * 0.8));
+            this.row_def_6_1.Height = new GridLength(15, GridUnitType.Star);
+            this.row_def_6_2.Height = new GridLength(35, GridUnitType.Star);
+            this.row_def_6_3.Height = new GridLength(15, GridUnitType.Star);
+            this.row_def_6_4.Height = new GridLength(35, GridUnitType.Star);
+
+            this.txt_bl_Zeitraum.FontSize = this.panel_1.Height * 0.08;
+            this.border_4.Height = this.panel_1.Height * 0.3;
+            this.txt_bl_IP.FontSize = this.panel_1.Height * 0.08;
+            this.border_5.Height = this.panel_1.Height * 0.3;
+            this.border_5.Margin = new Thickness(0, 45, 0, 0);
+
+            this.icon_DateRange_1.Width = this.border_4.Width * 0.05;
+            this.icon_DateRange_1.Height = this.border_4.Height * 0.7;
+
+            this.txt_bl_von.Width = this.border_4.Width * 0.1;
+            this.txt_bl_von.Height = this.border_4.Height * 0.7;
+            this.txt_bl_von.FontSize = this.border_4.Height * 0.3;
+
+            this.view_1.Height = this.border_4.Height * 0.6;
+            this.datepicker_first.FontSize = this.border_4.Height * 0.15;
+
+            this.icon_TimeRange_1.Height = this.border_4.Height * 0.6;
+            this.icon_TimeRange_1.FontSize = this.border_4.Height * 0.3;
+            this.icon_TimeRange_1.Width = this.border_4.Width * 0.1;
+
+            this.txt_bl_bis.Width = this.border_4.Width * 0.1;
+            this.txt_bl_bis.Height = this.border_4.Height * 0.7;
+            this.txt_bl_bis.FontSize = this.border_4.Height * 0.3;
+
+            //this.datepicker_first.LayoutTransform = new ScaleTransform(this.border_4.Height * 0.7, this.border_4.Height * 0.7);
+
+
+
+
+            Loaded += (s, e) =>
+            {
+                FrameworkElement firstColumnRef = (FrameworkElement)FindName("FirstColumnRef");
+                //this.ckbx_IP.LayoutTransform = new ScaleTransform(test_1, this.panel_FirstAnalyse.Height / 140.0);
+                this.test1.Text = (this.panel_FirstAnalyse.Width * 0.0060606).ToString() + " " + testings.ToString();
+            };
+            
 
 
         }
