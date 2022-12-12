6c7ab4d22feb348c0dae7769797a9db185b11268
jurijfischer212105@gmail.com <jurijfischer212105@gmail.com>
2022-12-12 16:35:28 +0100

    -

--- WpfApp1fewfwef/DBHandler.cs
+++ WpfApp1fewfwef/DBHandler.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Windows;
 using System.Data;
 using System.Collections.Generic;
 using System.Text;
@@ -11,48 +12,54 @@ namespace WpfApp1fewfwef
     {
         public static void NonQuery(string squery)
         {
-            string connection = @"Data Source=" + WpfApp1fewfwef.Properties.Settings.Default.DB_FILE + ";Version=3;New=True;Compress=True;";
-            SQLiteConnection sqlite_conn = new SQLiteConnection(connection);           
+            if (WpfApp1fewfwef.Properties.Settings.Default.DB_FILE != "")
+            {
+                string connection = @"Data Source=" + WpfApp1fewfwef.Properties.Settings.Default.DB_FILE + ";Version=3;New=True;Compress=True;";
+                SQLiteConnection sqlite_conn = new SQLiteConnection(connection);
 
-            sqlite_conn.Open();
-            var SqliteCmd = sqlite_conn.CreateCommand();
-            SqliteCmd.CommandText = squery;
-            SqliteCmd.ExecuteNonQuery();
-            sqlite_conn.Close();
+                sqlite_conn.Open();
+                var SqliteCmd = sqlite_conn.CreateCommand();
+                SqliteCmd.CommandText = squery;
+                SqliteCmd.ExecuteNonQuery();
+                sqlite_conn.Close();
+            }
+            else { MessageBox.Show("Bitte zuerst eine Datenbank wählen!"); }
+
         }
 
         public static DataTable ExecuteQuery(string squery)
         {
             SQLiteDataReader sqlite_datareader;
             DataTable queryDG = new DataTable();
-
-            string connection = @"Data Source=" + WpfApp1fewfwef.Properties.Settings.Default.DB_FILE + ";Version=3;New=True;Compress=True;";
-            SQLiteConnection sqlite_conn = new SQLiteConnection(connection);
-
-            sqlite_conn.Open();
-            var SqliteCmd = sqlite_conn.CreateCommand();
-            SqliteCmd.CommandText = squery;
-            sqlite_datareader = SqliteCmd.ExecuteReader();
-
-            for (int i = 0; i < sqlite_datareader.FieldCount; i++)
+            if (WpfApp1fewfwef.Properties.Settings.Default.DB_FILE != "")
             {
-                queryDG.Columns.Add(sqlite_datareader.GetName(i));
-            }
+                string connection = @"Data Source=" + WpfApp1fewfwef.Properties.Settings.Default.DB_FILE + ";Version=3;New=True;Compress=True;";
+                SQLiteConnection sqlite_conn = new SQLiteConnection(connection);
 
-            sqlite_datareader.Close();
-            sqlite_datareader = SqliteCmd.ExecuteReader();
+                sqlite_conn.Open();
+                var SqliteCmd = sqlite_conn.CreateCommand();
+                SqliteCmd.CommandText = squery;
+                sqlite_datareader = SqliteCmd.ExecuteReader();
 
-            while (sqlite_datareader.Read())
-            {
-                List<string> dataRows = new List<string>();
                 for (int i = 0; i < sqlite_datareader.FieldCount; i++)
                 {
-                    dataRows.Add(sqlite_datareader.GetValue(i).ToString());
+                    queryDG.Columns.Add(sqlite_datareader.GetName(i));
                 }
-                queryDG.Rows.Add(dataRows.ToArray());
-            }
-            sqlite_conn.Close();
 
+                sqlite_datareader.Close();
+                sqlite_datareader = SqliteCmd.ExecuteReader();
+
+                while (sqlite_datareader.Read())
+                {
+                    List<string> dataRows = new List<string>();
+                    for (int i = 0; i < sqlite_datareader.FieldCount; i++)
+                    {
+                        dataRows.Add(sqlite_datareader.GetValue(i).ToString());
+                    }
+                    queryDG.Rows.Add(dataRows.ToArray());
+                }
+                sqlite_conn.Close();
+            }else { MessageBox.Show("Bitte zuerst eine Datenbank wählen!"); }
             return queryDG;
         }
     }
